848fe14810fd590f0262c652ab910486e682d975
huangzlchannelsoft <huangzl@channelsoft.com>
2019-08-16 17:41:00 +0800

    fix client net bug.

--- main.go
+++ main.go
@@ -27,17 +27,17 @@ var (
 )
 
 type Config struct {
-	StrategyInfoPath 	string 	`yaml:"strategyInfoPath"`
-	TimeMinInterva		int64	`yaml:"timeMinInterva"`
-	CdrCommaTotal 		int 	`yaml:"cdrCommaTotal"`
-	FixedLineReg 		string 	`yaml:"fixedLineReg"`
-	ConAbnormal 		int		`yaml:"conAbnormal"`
-	Percentage      	float64 `yaml:"percentage"`
-	MobileReg 			string 	`yaml:"mobileReg"`
-	Version 			string 	`yaml:"version"`
-	Logfile 			bool   	`yaml:"logfile"`
-	Bakdays 			int    	`yaml:"bakdays"`
-	Nid     			string 	`yaml:"nid"`
+	StrategyInfoPath string  `yaml:"strategyInfoPath"`
+	TimeMinInterva   int64   `yaml:"timeMinInterva"`
+	CdrCommaTotal    int     `yaml:"cdrCommaTotal"`
+	FixedLineReg     string  `yaml:"fixedLineReg"`
+	ConAbnormal      int     `yaml:"conAbnormal"`
+	Percentage       float64 `yaml:"percentage"`
+	MobileReg        string  `yaml:"mobileReg"`
+	Version          string  `yaml:"version"`
+	Logfile          bool    `yaml:"logfile"`
+	Bakdays          int     `yaml:"bakdays"`
+	Nid              string  `yaml:"nid"`
 }
 
 var gCfg Config
@@ -73,24 +73,26 @@ CollectCdr -TransmitCdr-> ParseCdr -TransmitAlarmCdr-
 func main() {
 	flag.Parse()
 
-	SetPhonePropertyUri(*phoneIspUri, *phoneProUri)
-	SetPhonePropertyFile(*ispTxtFile, *proXlsxFile)
-
 	ctx, cancel := context.WithCancel(context.Background())
-	go TrickerDeamon(ctx)
-	go PromethuesClient(true, *pushGateWayUri, gCfg.Nid, 60)
-
-	go TransmitCdr(*as, *svrAddr)
-	go TransmitAlarmCdr(*alarmUri)
-	go TransmitMonCdr()
 
 	if *as == "client" {
 		go CollectCdr(*cdrPath, SendCdr)
-	} else if *as == "server" {
-		go ParseCdr(RecvCdr, SendAlarm)
-	} else if *as == "singleton" {
-		go CollectCdr(*cdrPath, SendCdr)
-		go ParseCdr(RecvCdr, SendAlarm)
+	} else {
+		SetPhonePropertyUri(*phoneIspUri, *phoneProUri)
+		SetPhonePropertyFile(*ispTxtFile, *proXlsxFile)
+		go TrickerDeamon(ctx)
+		go PromethuesClient(true, *pushGateWayUri, gCfg.Nid, 60)
+
+		go TransmitCdr(*as, *svrAddr)
+		go TransmitAlarmCdr(*alarmUri)
+		go TransmitMonCdr()
+
+		if *as == "server" {
+			go ParseCdr(RecvCdr, SendAlarm)
+		} else if *as == "singleton" {
+			go CollectCdr(*cdrPath, SendCdr)
+			go ParseCdr(RecvCdr, SendAlarm)
+		}
 	}
 
 	c := make(chan os.Signal)
